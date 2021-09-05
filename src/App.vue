bdbbae8cb0ca64004ae21daa96839e1b98ae97d6
Ankit <ankit.sin099@gmail.com>
2021-09-05 16:31:29 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer Game</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score"/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
   }
 }
 </script>
@@ -20,7 +43,21 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="score">
+      <p>Reaction Time: {{ score }} ms</p>
+      <p class="rank">{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if(this.score < 250) {
+            this.rank = '😲Flash or What'
+        }
+        else if(this.score < 400) {
+            this.rank = '😮Rapid Reflexes'
+        }
+        else {
+            this.rank = '😢Snail pace....'
+        }
+    }
+}
+</script>
+
+<style>
+    .score > p {
+        font-style: italic;
+        font-weight: bold;
+        font-size: 1.2em;
+    }
+
+    .rank {
+        font-size: 1.8em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
